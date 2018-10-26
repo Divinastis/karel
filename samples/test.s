986bcb2e92f62eec1d48a4e6e0502b2ddab8b457
Divi <juliendupiol@gmail.com>
2018-10-26 12:02:50 +0200

    something

--- samples/test.s
+++ samples/test.s
@@ -7,14 +7,20 @@ L2:
 L3:
 	invoke 1, 0, 0
 L4:
-	invoke 1, 0, 0
+	invoke 11, 0, 0
 L5:
 	invoke 1, 0, 0
 L6:
-	invoke 1, 0, 0
+	invoke 11, 0, 0
 L7:
 	invoke 1, 0, 0
 L8:
 	invoke 1, 0, 0
 L9:
+	invoke 1, 0, 0
+L10:
+	invoke 11, 0, 0
+L11:
+	invoke 1, 0, 0
+L12:
 	invoke 2, 0, 0
